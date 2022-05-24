9c9e25fa19cdb51be06afd3bc25968408a2c28cc
matheuseterno <matheus.eterno@outlook.com.br>
2022-05-24 16:32:07 -0300

    Criação do projeto

---
+++ src/main/java/br/com/portifolio/planeja/PlanejaApplication.java
@@ -0,0 +1,13 @@
+package br.com.portifolio.planeja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanejaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portifolio/planeja/PlanejaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portifolio.planeja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
