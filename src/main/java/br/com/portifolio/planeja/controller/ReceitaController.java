c103fdb6e162005f34d2dc29082032f0f0398b54
matheuseterno <matheus.eterno@outlook.com.br>
2022-05-28 12:01:00 -0300

    Detalhamento de Receita

--- src/main/java/br/com/portifolio/planeja/controller/ReceitaController.java
+++ src/main/java/br/com/portifolio/planeja/controller/ReceitaController.java
@@ -1,9 +1,16 @@
 package br.com.portifolio.planeja.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.portifolio.planeja.dto.ReceitaDto;
 import br.com.portifolio.planeja.model.Receita;
 import br.com.portifolio.planeja.repository.ReceitaRepository;
 
@@ -17,5 +24,19 @@ public class ReceitaController {
 	public List<Receita> lista() {
 		List<Receita> receitas = receitaRepository.findAll();
 		return receitas;
+	}
+	
+	@GetMapping("/receitas/{id}")
+	public ResponseEntity<ReceitaDto> detalhar(@PathVariable("id") Long id) {
+		Optional<Receita> receita = receitaRepository.findById(id);
+		if(receita.isPresent()) {
+			return ResponseEntity.ok(new ReceitaDto(receita.get()));
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Receita> cadastrar (){
+		return null;
 	}
 }
