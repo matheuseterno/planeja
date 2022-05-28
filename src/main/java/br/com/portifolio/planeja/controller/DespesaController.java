a969e195355adec4a4d5af0930468b10802fdaa5
matheuseterno <matheus.eterno@outlook.com.br>
2022-05-28 19:08:14 -0300

    Detalhamento de despesa

--- src/main/java/br/com/portifolio/planeja/controller/DespesaController.java
+++ src/main/java/br/com/portifolio/planeja/controller/DespesaController.java
@@ -1,9 +1,12 @@
 package br.com.portifolio.planeja.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.portifolio.planeja.dto.DespesaDto;
@@ -20,5 +23,14 @@ public class DespesaController {
 	public List<DespesaDto> lista(){
 		List<Despesa> despesas = despesaRepository.findAll();
 		return DespesaDto.converter(despesas);
+	}
+	
+	@GetMapping("/despesas/{id}")
+	public ResponseEntity<DespesaDto> detalhar(@PathVariable("id") Long id) {
+		Optional<Despesa> despesa = despesaRepository.findById(id);
+		if(despesa.isPresent()) {
+			return ResponseEntity.ok(new DespesaDto(despesa.get()));
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
